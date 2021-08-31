144cf091a9c6caaba5b21bb67a2be69ad0dc3588
Byron Smith <byrontre@live.unc.edu>
2021-08-30 20:01:31 -0400

    exercises

---
+++ exercises/ex01/numeric_operators.py
@@ -0,0 +1,10 @@
+"""Numerical Operators Assignment."""
+
+__author__ = "730345086"
+
+input("Left-hand side: ")
+input("Right-hand side: ")
+print("3 " "**" " 6" " is 729") 
+print("3 " "/" " 6" " is 0.5")
+print("3 " "//" " 6" " is 0")
+print("3 " "%" " 6" " is 3")
