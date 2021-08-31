8bc4edf36c00877f72453bf0c377bc566a7b7506
Byron Smith <byrontre@live.unc.edu>
2021-08-31 19:53:14 -0400

    exercises

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -3,6 +3,7 @@
 __author__ = "730345086"
 
 name: str = input("Who might you be? ")
+
 print("Well, " + name + ", you need to get into it!")
 print("You have to believe in yourself more than anything, " + name + "!")
 print(name + ", chase that degree and secure the bag! ")
