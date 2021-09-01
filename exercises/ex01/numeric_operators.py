8658662485b1906aa0a3b79c165f7d31d6009e69
Byron Smith <byrontre@live.unc.edu>
2021-08-31 20:40:09 -0400

    exercises

--- exercises/ex01/numeric_operators.py
+++ exercises/ex01/numeric_operators.py
@@ -5,12 +5,12 @@ __author__ = "730345086"
 su_ga: str = input("Left-hand side: ")
 tae_hyung: str = input("Right-hand side: ")
 
-jimin = su_ga + " ** " + tae_hyung + " is 729 " 
-jin = su_ga + " / " + tae_hyung + " is 0.5 "
-rapmon = su_ga + " // " + tae_hyung + " is 0 "
-jungkook = su_ga + " % " + tae_hyung + " is 3 "
+expon = su_ga + " ** " + tae_hyung + " is 729" 
+divis = su_ga + " / " + tae_hyung + " is 0.5"
+int_d = su_ga + " // " + tae_hyung + " is 0"
+rmndr = su_ga + " % " + tae_hyung + " is 3"
 
-print(jimin)
-print(jin)
-print(rapmon)
-print(jungkook)
+print(expon)
+print(divis)
+print(int_d)
+print(rmndr)
--- exercises/ex01/relational_operators.py
+++ exercises/ex01/relational_operators.py
@@ -5,12 +5,12 @@ __author__ = "730345086"
 tae_yeon: str = input("Left-hand side: ")
 tif_fany: str = input("Right-hand side: ")
 
-sica = tae_yeon + " < " + tif_fany + " is False"
-yuri = tae_yeon + " >= " + tif_fany + " is True"
-yoona = tae_yeon + " == " + tif_fany + " is False"
-sunny = tae_yeon + " != " + tif_fany + " is True"
+lessr = tae_yeon + " < " + tif_fany + " is False"
+atlst = tae_yeon + " >= " + tif_fany + " is True"
+eqlto = tae_yeon + " == " + tif_fany + " is False"
+nteql = tae_yeon + " != " + tif_fany + " is True"
 
-print(sica)
-print(yuri)
-print(yoona)
-print(sunny)
+print(lessr)
+print(atlst)
+print(eqlto)
+print(nteql)
