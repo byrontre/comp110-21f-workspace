28199ff8c47a57827a2bbfa4a17815b6e25ff7ab
Byron Smith <byrontre@live.unc.edu>
2021-08-31 20:16:14 -0400

    exercises

--- exercises/ex01/relational_operators.py
+++ exercises/ex01/relational_operators.py
@@ -5,12 +5,12 @@ __author__ = "730345086"
 tae_yeon: str = input("Left-hand side: ")
 tif_fany: str = input("Right-hand side: ")
 
-jessica = tae_yeon + " < " + tif_fany + " is False"
+sica = tae_yeon + " < " + tif_fany + " is False"
 yuri = tae_yeon + " >= " + tif_fany + " is True"
 yoona = tae_yeon + " == " + tif_fany + " is False"
 sunny = tae_yeon + " != " + tif_fany + " is True"
 
-print(jessica)
+print(sica)
 print(yuri)
 print(yoona)
 print(sunny)
