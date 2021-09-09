22dbe18649b0d0b2bcd448692e6f61539bda9b5a
Byron Smith <byrontre@live.unc.edu>
2021-09-08 21:52:55 -0400

    exercises

--- exercises/ex02/count_letters.py
+++ exercises/ex02/count_letters.py
@@ -2,5 +2,14 @@
 
 __author__ = "730345086"
 
+i: int = 0
 
-# Begin your solution here...
+alpha: str = input("What letter do you want to search for?: ")
+phrase: str = input("Enter a word: ")
+position = alpha
+
+while i < len(phrase):
+    amnt: int = len(alpha)
+    line: str = phrase[amnt]
+    print("Count: " + line)
+    i = len(phrase)
