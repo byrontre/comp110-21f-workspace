057724ee89452000141ca659c3a491daa244442e
Byron Smith <byrontre@live.unc.edu>
2021-09-13 00:14:49 -0400

    exercises

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,16 @@
-"""Finding duplicate letters in a word."""
+"""Where Them Doubles At."""
 
-__author__ = "123456789"
+__author__ = "730345086"
+
+
+select: str = input("Enter a word: ")
+tag: bool = False
+i: int = 0
+
+while i < len(select):
+    print("안녕하세요, 여러분!!! ")
+    k: int = i + 1
+    while k < len(select):
+        print("비행기로 여행하기 싫어요. ")
+
+print("지금은 소녀시대! ")
--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -1,6 +1,17 @@
 """Drawing forests in a loop."""
 
-__author__ = "123456789"
+__author__ = "730345086"
 
-# The string constant for the pine tree emoji
+from random import randint
+
 TREE: str = '\U0001F332'
+
+pine: int = int(input("Depth: "))
+x: int = int(randint(1, 1000))
+
+if x > pine:
+    print("저희가 분명하게 생가해야 했잖아요")
+else: 
+    print("솔직히 바로 이 순건까지 아직도 전혀 모르겠지만 웃기가 훨씬 더 쉽니다. ")
+
+print("놀아서 감사합니다!!! ")
