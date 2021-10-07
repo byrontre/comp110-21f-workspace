0e5dabdf78620c0e1b9b758b4d82f0b048198833
Byron Smith <byrontre@live.unc.edu>
2021-10-06 20:08:02 -0400

    Progress on ex05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,37 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730345086"
 
-# Define your functions below
+
+def only_evens(x: list[int]) -> list[int]:
+    """Find the Even Ints."""
+    i: int = 0
+    while i < len(x):
+        read: int = x[i]
+        if read % 2 > 0:
+            x.pop(i)
+        i += 1
+    return x
+
+
+def sub(a: list[int], b: int, c: int) -> list[int]:
+    """Let's Make a List."""
+    sub_set: list[int] = []
+    if len(a) == 0 or b > len(a) or c <= 0:
+        return sub_set
+    else:
+        for i in a:
+            if b < 0 or b > 0 or c > len(a):
+                if i >= b and i <= c - 1:
+                    sub_set.append(i)
+    return sub_set
+            
+
+def concat(one: list[int], two: list[int]) -> list[int]:
+    """Put It All Together."""
+    all_in: list[int] = []
+    for i in one:
+        all_in.append(i)
+    for i in two:
+        all_in.append(i)
+    return all_in
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,24 @@
 """Unit tests for list utility functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+from exercises.ex05.utils import only_evens, sub, concat
+
+__author__ = "730345086"
+
+
+def test_only_evens_full() -> None:
+    x: list[int] = [3, 6, 9, 12, 44]
+    assert only_evens(x) == [6, 12, 44]
+
+
+def test_sub_void() -> None:
+    a: list[int] = []
+    b: int = 0
+    c: int = 0
+    assert sub(a, b, c) == []
+
+
+def test_concat_connect_lists() -> None:
+    one: list[int] = [11, 10, 3, 25]
+    two: list[int] = [2, 8, 10, 19]
+    assert concat(one, two) == [11, 10, 3, 25, 2, 8, 10, 19]
