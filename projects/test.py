ded23fc0397d8ee61603505b66b97a4e267056b7
Byron Smith <byrontre@live.unc.edu>
2021-10-06 22:02:06 -0400

    projects

--- projects/test.py
+++ projects/test.py
@@ -1,7 +1,13 @@
-"""JThis is the Testing Arena for Things to See How They Run."""
+"""This is the Testing Arena for Things to See How They Run."""
 
-whatever: str = '\U0001F339'
-print(whatever)
 
-coco: str = '\U0001F340'
-print(coco)
+def sub(a: list[int], b: int, c: int) -> list[int]:
+    """Let's Make a List."""
+    sub_set: list[int] = []
+    if len(a) == 0 or b >= len(a) or c <= 0:
+        return sub_set
+    for indices in a:
+        if b < len(a) or c > len(a):
+            if indices > b and indices <= c - 1:
+                sub_set.append(indices)
+    return sub_set
