4212ca6bbbccc10eab05e32cc1e476205e322bec
Byron Smith <byrontre@live.unc.edu>
2021-10-14 15:34:29 -0400

    projects

--- projects/test.py
+++ projects/test.py
@@ -1,13 +1,11 @@
 """This is the Testing Arena for Things to See How They Run."""
 
 
-def sub(a: list[int], b: int, c: int) -> list[int]:
-    """Let's Make a List."""
-    sub_set: list[int] = []
-    if len(a) == 0 or b >= len(a) or c <= 0:
-        return sub_set
-    for indices in a:
-        if b < len(a) or c > len(a):
-            if indices > b and indices <= c - 1:
-                sub_set.append(indices)
-    return sub_set
+bangtan: dict[str, str] = dict()
+
+bangtan["butter"] = "iconic"
+bangtan["dynamite"] = "meh"
+bangtan["permission to dance"] = "who"
+
+for songs in bangtan:
+    value: str = bangtan[songs]
