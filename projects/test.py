42d7920eadd307a9d8ac667a8c26f5028a834c01
Byron Smith <byrontre@live.unc.edu>
2021-10-21 13:59:26 -0400

    projects

--- projects/test.py
+++ projects/test.py
@@ -1,1 +1,29 @@
 """This is the Testing Arena for Things to See How They Run."""
+
+
+case_bot: dict[int, str] = {}
+
+snatch: str = '\U0001F430'
+catch: str = '\U0001F419'
+latch: str = '\U0001F436'
+
+case_bot[10] = snatch
+case_bot[12] = catch
+case_bot[14] = latch
+
+print(case_bot)
+
+case_bot.pop(12)
+
+filler: str = '\U0001F456'
+
+case_bot[16] = filler
+
+print(case_bot)
+
+safe_bot: dict[str, int] = {}
+
+for keys in case_bot:
+    safe_bot[case_bot[keys]] = keys
+
+print(safe_bot)
