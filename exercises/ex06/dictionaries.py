39e9e279d23be163cc6fddd61ad96194938d772e
Byron Smith <byrontre@live.unc.edu>
2021-10-20 21:50:04 -0400

    Progress on ex06

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,44 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730345086"
 
-# Define your functions below
+
+def invert(trace: dict[str, str]) -> dict[str, str]:
+    """Flip the Dictionary."""
+    filler: dict[str, str] = {}
+    for keys in trace:
+        filler[trace[keys]] = keys
+        if keys in filler:
+            raise KeyError("Access denied: Duplicate keys detected.")
+    return filler
+            
+
+def favorite_colors(red: dict[str, str]) -> str:
+    """Pick a Color, Return the Most Frequent."""
+    score: dict[str, int] = {}
+    this_color: str = ""
+    counter: int = 0
+    for keys in red:
+        score[red[keys]] = 1
+        if red[keys] in score:
+            score[red[keys]] += 1
+    for keys in score:
+        if score[keys] > counter:
+            counter = score[keys]
+            this_color = keys
+    return this_color
+        
+
+def count(value: list[str]) -> dict[str, int]:
+    """If It's There, Add One... if Not, Move On."""
+    new_list: dict[str, int] = dict()
+    i: int = 0
+    while i < len(value):
+        if value[i] in new_list:
+            new_list[value[i]] += 1
+        else: 
+            new_list[value[i]] = 1
+        i += 1
+    return new_list
+
+    
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,58 @@
 """Unit tests for dictionary functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
+from exercises.ex06.dictionaries import invert, favorite_colors, count
 
-__author__ = "123456789"
+__author__ = "730345086"
+
+
+def test_invert_bland() -> None:
+    """Edge Case with No Values."""
+    trace: dict[str, str] = {}
+    assert invert(trace) == {}
+
+
+def test_invert_small_set() -> None:
+    """Use Case with Minimal Key & Values."""
+    trace: dict[str, str] = {"a": "tuna", "b": "salmon"}
+    assert invert(trace) == {"tuna": "a", "salmon": "b"}
+
+
+def test_invert_completion() -> None:
+    """Use Case with Many Keys & Values."""
+    trace: dict[str, str] = {"c": "bunny", "d": "turtle", "e": "squid", "f": "mouse", "g": "owl"}
+    assert invert(trace) == {"bunny": "c", "turtle": "d", "squid": "e", "mouse": "f", "owl": "g"}
+
+
+def test_favorite_colors_busted() -> None:
+    """Edge Case with No Dict."""
+    red: dict[str, str] = {}
+    assert favorite_colors(red) == ""
+    
+
+def test_favorite_colors_simple() -> None:
+    """Use Case with Basic Dict."""
+    red: dict[str, str] = {"sugar": "red", "milk": "blue", "butter": "red"}
+    assert favorite_colors(red) == "red"
+
+
+def test_favorite_colors_valhalla() -> None:
+    """Use Case with Full Dict."""
+    red: dict[str, str] = {"earth": "yellow", "wind": "pink", "fire": "green", "water": "pink", "lightning": "yellow"}
+    assert favorite_colors(red) == "yellow"
+
+
+def test_count_lukewarm() -> None:
+    """Edge Case with No List."""
+    value: list[str] = []
+    assert count(value) == {}
+
+
+    """Use Case with Minor List."""
+    value: list[str] = ["tea", "tea", "egg", "bacon"]
+    assert count(value) == {"tea": 2, "egg": 1, "bacon": 1}
+
+
+def test_count_fuego() -> None:
+    """Use Case with Full List."""
+    value: list[str] = ["chile", "jalapeno", "ghost", "bell", "jalapeno", "pimento", "jalapeno", "bell"]
+    assert count(value) == {"chile": 1, "jalapeno": 3, "ghost": 1, "bell": 2, "pimento": 1}
