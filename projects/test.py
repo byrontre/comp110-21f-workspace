456f8536b5d9c8064753baba53ab7db5871fab47
Byron Smith <byrontre@live.unc.edu>
2021-10-20 21:50:11 -0400

    projects

--- projects/test.py
+++ projects/test.py
@@ -1,11 +1,1 @@
 """This is the Testing Arena for Things to See How They Run."""
-
-
-bangtan: dict[str, str] = dict()
-
-bangtan["butter"] = "iconic"
-bangtan["dynamite"] = "meh"
-bangtan["permission to dance"] = "who"
-
-for songs in bangtan:
-    value: str = bangtan[songs]
