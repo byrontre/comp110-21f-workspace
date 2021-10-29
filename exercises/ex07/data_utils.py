88ee4c8c992f4f633423e114e418d6ce4850d5a9
Byron Smith <byrontre@live.unc.edu>
2021-10-29 00:21:43 -0400

    Progress on ex07

--- exercises/ex07/data_utils.py
+++ exercises/ex07/data_utils.py
@@ -1,5 +1,54 @@
 """Utility functions."""
 
-__author__ = "123456789"
+__author__ = "730345086"
 
-# Define your functions below
+from csv import DictReader
+
+
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
+    """Read the rows of a csv file into a 'table'."""
+    result: list[dict[str, str]] = []
+    file_handle = open(filename, "r", encoding="utf8")
+    csv_reader = DictReader(file_handle)
+    for row in csv_reader:
+        result.append(row)
+    file_handle.close()
+    return result
+
+
+def column_values(table: list[dict[str, str]], column: str) -> list[str]:
+    """Produce a list[str] of all values in a single column."""
+    result: list[str] = []
+    for row in table:
+        item: str = row[column]
+        result.append(item)
+    return result
+
+
+def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Transform a row-oriented able into a column-oriented table."""
+    result: dict[str, list[str]] = {}
+
+    first_row: dict[str, str] = row_table[0]
+    for column in first_row:
+        result[column] = column_values(row_table, column)
+        
+    return result
+
+
+def head(x: dict[str, list[str]], y: int) -> dict[str, list[str]]:
+    """Return a Column with N Rows."""
+    stem: dict[str, list[str]] = {}
+    return stem
+
+
+def select(a: dict[str, list[str]], b: list[str]) -> dict[str, list[str]]:
+    """Produce Subset of Columns from Total Colum Data."""
+    reject: dict[str, list[str]] = {}
+    return reject 
+
+
+def concat(one: dict[str, list[str]], two: dict[str, list[str]]) -> dict[str, list[str]]:
+    """Combine Two Dicts into One Dict."""
+    combo: dict[str, list[str]] = {}
+    return combo
