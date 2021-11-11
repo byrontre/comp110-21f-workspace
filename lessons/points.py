9434d320097671f48402be4e75634956c1e744e3
Byron Smith <byrontre@live.unc.edu>
2021-11-10 21:13:12 -0500

    lessons

--- lessons/points.py
+++ lessons/points.py
@@ -18,14 +18,42 @@ class Point:
         self.y *= factor
 
     def scale(self, factor: float) -> Point:
-        """Immutable: multiplies components by factor without mutation."""
-        x: float = self.x * factor
-        y: float = self.y * factor
-        scaled_point: Point = Point(x, y)
-        return scaled_point
+        """Scale a Point's attributes by factor."""
+        return Point(self.x * factor, self.y * factor)
+
+    def __str__(self) -> str:
+        """Produce a str representation of a Point for Humans."""
+        return f"({self.x}, {self.y})"
+
+    def __repr__(self) -> str:
+        """Produce a str representation of a Point for Python."""
+        return f"Point ({self.x}, {self.y})"
+
+    def __mul__(self, factor: float) -> Point:
+        """Overload the Multiplication Operator for Point * Float."""
+        return Point(self.x * factor, self.y * factor)
+
+    def __add__(self, rhs: Point) -> Point:
+        """This One."""
+        print("__add__ was called!")
+        return Point(self.x * rhs.x, self.y * rhs.y)
+
+    def __getitem__(self, index: int) -> float:
+        """Overload the subscription notation."""
+        if index == 0:
+            return self.x
+        elif index == 1:
+            return self.y
+        else:
+            raise IndexError
 
 
-p0: Point = Point(1.0, 2.0)
-p1: Point = p0.scale(2.0)
-print(f"({p0.x}, {p0.y})")
-print(f"({p1.x}, {p1.y}) ")
+a: Point = Point(1.0, 2.0)
+b: Point = a * 2.0
+c: Point = a + b
+print(f"a: {a}")
+print(f"b: {b}")
+print(f"c: {c}")
+
+print(a[0])
+print(a[1])
